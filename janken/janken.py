77bd05f352e12f891698b7d0eaebb027574859ba
cmoqrin <kato.teiji@classmethod.jp>
2019-11-29 16:43:09 +0900

    test4

--- janken/janken.py
+++ janken/janken.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
 import random
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -9,16 +9,16 @@ def janken():
     if request.method == 'POST':
         client_hand = request.form['hand']
         match["client_hand"] = client_hand
-        janken_result = pc_choice(client_hand, match)
+        pc_choice(client_hand, match)
 
         return render_template('result.html', client_hand=match["client_hand"],
-                               pc_hand=match["pc_hand"], janken_result=match["result"])
+                               pc_hand=match["pc_hand"],
+                               janken_result=match["result"])
 
     return render_template('index.html')
 
 
 def pc_choice(client_hand, match):
-    client_hand = client_hand
     pc_choices = ["グー", "チョキ", "パー"]
     pc_hand = random.choice(pc_choices)
     match["pc_hand"] = pc_hand
@@ -30,30 +30,24 @@ def pc_choice(client_hand, match):
     if client_hand == pc_hand:
         match["result"] = draw
         return match
-    else:
-        if client_hand == "グー":
-            if pc_hand == "チョキ":
-                match["result"] = win
-                return match
-            else:
-                match["result"] = lose
-                return match
 
-        elif client_hand == "チョキ":
-            if pc_hand == "パー":
-                match["result"] = win
-                return match
-            else:
-                match["result"] = lose
-                return match
-
+    if client_hand == "グー":
+        if pc_hand == "チョキ":
+            match["result"] = win
         else:
-            if pc_hand == "グー":
-                match["result"] = win
-                return match
-            else:
-                match["result"] = lose
-                return match
+            match["result"] = lose
+
+    elif client_hand == "チョキ":
+        if pc_hand == "パー":
+            match["result"] = win
+        else:
+            match["result"] = lose
+    else:
+        if pc_hand == "グー":
+            match["result"] = win
+        else:
+            match["result"] = lose
+    return match
 
 
 if __name__ == "__main__":
